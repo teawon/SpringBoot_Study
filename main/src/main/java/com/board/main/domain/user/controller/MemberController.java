ba66b5e442d4993202b1d175effcb2d43aff09db
teawon <xodnjsvkvk@naver.com>
2022-04-02 18:40:56 +0900

    feat : 회원가입 (memory)

--- main/src/main/java/com/board/main/domain/user/controller/MemberController.java
+++ main/src/main/java/com/board/main/domain/user/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.board.main.domain.user.controller;
 
+import com.board.main.domain.user.entity.Member;
 import com.board.main.domain.user.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -13,4 +16,22 @@ public class MemberController {
     public MemberController(MemberService memberService){
         this.memberService = memberService;
     }
+
+    @GetMapping("ourboard/users/signup")
+    public String createSignupForm(){
+        return "users/signupForm";
+    }
+
+    @PostMapping("/ourboard/users/signup")
+    public String creat(SignupForm form){
+        Member member = new Member();
+        member.setUserID(form.getUserId());
+        member.setPassword(form.getPassword());
+        member.setUserName(form.getUserName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
 }
---
+++ main/src/main/java/com/board/main/domain/user/controller/SignupForm.java
@@ -0,0 +1,33 @@
+package com.board.main.domain.user.controller;
+
+public class SignupForm {
+    private String userId;
+    private String password;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    private String userName;
+
+}
