18a9fd5b861de6b84927a84f3961e487481efe8f
teawon <xodnjsvkvk@naver.com>
2022-04-02 19:06:23 +0900

    feat : 회원 정보 확인

--- main/src/main/java/com/board/main/domain/user/controller/MemberController.java
+++ main/src/main/java/com/board/main/domain/user/controller/MemberController.java
@@ -4,8 +4,11 @@ import com.board.main.domain.user.entity.Member;
 import com.board.main.domain.user.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -32,6 +35,13 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/ourboard/users/view")
+    public String List(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "users/view";
     }
 
 }
